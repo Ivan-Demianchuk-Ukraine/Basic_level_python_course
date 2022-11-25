fb14ba03245d8f2e1e44a0deb2ca97aa415fa144
IvanDemianchuk <energy.redler@gmail.com>
2022-11-25 14:19:28 +0300

    Added solutions for Task 3 and Task 5. Also updated solution for Task-4

--- home_work_1.py
+++ home_work_1.py
@@ -1,7 +1,21 @@
-print("""
-**    **          **    ****         ****     **
-**     **        **    **  **        ** **    **
-**      **      **    **    **       **  **   **
-**       **    **    **********      **   **  ** 
-**        **  **    **        **     **    ** **
-**         ****    **          **    **     ****""", end="")
+# Task-3
+# print("""'import antigravity'
+# 'import __hello__'
+# 'from __future__ import braces'""")
+
+
+# Task-4
+# print("""
+# **\t**          **    ****         ****     **\n**\t **        **    **  **        ** **    **
+# **\t  **      **    **    **       **  **   **\n**\t   **    **    **********      **   **  **
+# **\t    **  **    **        **     **    ** **\n**\t     ****    **          **    **     ****""", end="")
+
+# Task-5
+# print(r'''Escape sequences
+# \a      Bell (alert)
+# \b      Backspace
+# \n      New line
+# \t      Horizontal tab
+# \\      Backslash \
+# \"      Double quotation mark "
+# \'      Single quotation mark ' ''')
