c7e1bb9005ac0d4af8e677cd60ec482ca29c8f20
IvanDemianchuk <energy.redler@gmail.com>
2023-01-13 01:53:55 +0300

    added fix with naming of file

--- function_censor_HM_9.py
+++ function_censor_HM_9.py
@@ -2,7 +2,7 @@
 # функция ничего не возвращает, но в результате ее работы необходимо создать файл,
 # в котором слова из списка заменены шаблоном(звездочками например)
 def censor(file_name, lst_words):
-    file = open(f'{file_name}.txt', 'r')
+    file = open(f'{file_name}', 'r')
     opened_file = file.read()
     for i in lst_words:
         opened_file = opened_file.replace(i, '*' * len(i))
@@ -10,4 +10,4 @@ def censor(file_name, lst_words):
     file.close()
 
 
-censor('newfile', ['war', 'string'])
+censor('newfile.txt', ['war', 'string'])
