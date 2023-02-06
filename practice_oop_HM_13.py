587f445bdd412e56f38121205aa3ce8995ad82cd
IvanDemianchuk <energy.redler@gmail.com>
2023-02-06 17:20:26 +0300

    wrote the name of all classes that will be used in the program.

---
+++ practice_oop_HM_13.py
@@ -0,0 +1,62 @@
+class Transport:
+    pass
+
+
+class Bike(Transport):
+    pass
+
+
+class Automobile(Transport):
+    pass
+
+
+class Bus(Transport):
+    pass
+
+
+class Motorcycle(Bike):
+    pass
+
+
+class Truck(Bus):
+    pass
+
+
+class Animal:
+    pass
+
+
+class Bird(Animal):
+    pass
+
+
+class Reptile(Animal):
+    pass
+
+
+class Mammal(Animal):
+    pass
+
+
+class Eagle(Bird):
+    pass
+
+
+class Colibri(Bird):
+    pass
+
+
+class Snake(Reptile):
+    pass
+
+
+class Frog(Reptile):
+    pass
+
+
+class Whale(Mammal):
+    pass
+
+
+class Elephant(Mammal):
+    pass
